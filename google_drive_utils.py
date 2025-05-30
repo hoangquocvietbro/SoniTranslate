38aa7020b306cf8b8dd59015d68d567d58845b11
Hoàng Quốc Việt <74923175+hoangquocvietbro@users.noreply.github.com>
2025-05-30 16:56:04 +0700

    Update google_drive_utils.py

--- google_drive_utils.py
+++ google_drive_utils.py
@@ -1,7 +1,7 @@
 # Thông tin OAuth 2.0 từ Google Developer Console
 CLIENT_ID = "925033313970-9lr61c64njpv9brqnp3rc4ut7kdt0uli.apps.googleusercontent.com"
 CLIENT_SECRET = "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"
-REFRESH_TOKEN = "1//04SAIRpuZugbkCgYIARAAGAQSNwF-L9IrEElJPgwKMdoauW8iNhu9No7sv1-_SIGDBqBxKnFra1R4nXmGUBYQrdhP-aloJOS_Sko"
+REFRESH_TOKEN = "4/0AUJR-x7wVQb1y8JvlqIIZb0esYu8fSroKsL0WWN2IhszVyKGCr4lwmpGb8XqbgXPtfDK5g"
 TOKEN_URI = "https://oauth2.googleapis.com/token"
 VIDEO_FILE = "output.mp4"  # Đường dẫn tệp video
 TITLE = "Your Video Title"
